aa08be5827ab353bb5b903c4c9a39321d0d82af6
Pawel Dudko <paweldudko@outlook.com>
2022-09-17 15:41:35 +0200

    solution for task8

---
+++ src/pl/sdacademy/java/basic/exercises/day1/Task8.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.java.basic.exercises.day1;
+
+public class Task8 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            if(isPrimeNumber(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean isPrimeNumber(int number) {
+        if(number < 2) {
+            return false;
+        }
+
+        for(int i = 2; i  <= number/2; i++) {
+            if(number%i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
