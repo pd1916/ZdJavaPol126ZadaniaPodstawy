888fa1010022b07b7348cc32200c6e090539dce1
Pawel Dudko <paweldudko@outlook.com>
2022-09-17 15:06:53 +0200

    solution for task6

---
+++ src/pl/sdacademy/java/basic/exercises/day1/Task6.java
@@ -0,0 +1,35 @@
+package pl.sdacademy.java.basic.exercises.day1;
+
+public class Task6 {
+
+    public static void main(String[] args) {
+        printMultiplicationTable();
+        printMultiplicationTable(6, 2 ,5);
+    }
+
+    private static void printMultiplicationTable() {
+        for(int i = 1; i <= 4; i++) {
+            int result = 5 * i;
+            System.out.println("5 x " + i + " = " + result);
+        }
+    }
+
+    private static void printMultiplicationTable(int multiplier, int minMultiplicand, int maxMultiplicand) {
+//        for(; minMultiplicand <=maxMultiplicand; minMultiplicand++) {
+//            int result = multiplier * minMultiplicand;
+//            System.out.println(multiplier + " * " + minMultiplicand + " = " + result);
+//        }
+
+//        while(minMultiplicand <=maxMultiplicand) {
+//            int result = multiplier * minMultiplicand;
+//            System.out.println(multiplier + " * " + minMultiplicand + " = " + result);
+//            minMultiplicand++;
+//        }
+
+        do {
+            int result = multiplier * minMultiplicand;
+            System.out.println(multiplier + " * " + minMultiplicand + " = " + result);
+            minMultiplicand++;
+        }while(minMultiplicand <=maxMultiplicand);
+    }
+}
