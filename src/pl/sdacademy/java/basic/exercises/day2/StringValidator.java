77e6e254b12e96ace06f2e01976b547ff1ba5237
Pawel Dudko <paweldudko@outlook.com>
2022-09-18 10:29:19 +0200

    solution for task2 - day2

---
+++ src/pl/sdacademy/java/basic/exercises/day2/StringValidator.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.java.basic.exercises.day2;
+
+public class StringValidator {
+
+    public static boolean isValid(String input) {
+        return input != null && !input.isBlank();
+    }
+}
