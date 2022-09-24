5d03c273fda05f47a968500f220ba3423479ee0a
Pawel Dudko <paweldudko@outlook.com>
2022-09-24 13:50:00 +0200

    solution for task7 - day3

---
+++ src/pl/sdacademy/java/basic/exercises/day3/task7/Cat.java
@@ -0,0 +1,49 @@
+package pl.sdacademy.java.basic.exercises.day3.task7;
+
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void makeSound() {
+        System.out.println("Miau miau");
+    }
+
+    public void eatMice(int mice) {
+        System.out.println("I ate " + mice + " mice.");
+    }
+
+    public void print() {
+        /*
+        oba zapisy są równoważne
+        System.out.println(this)
+        System.out.println(this.toString())
+         */
+        //System.out.println(this);
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ src/pl/sdacademy/java/basic/exercises/day3/task7/Main.java
@@ -0,0 +1,27 @@
+package pl.sdacademy.java.basic.exercises.day3.task7;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Cat cat = new Cat("Mruczek");
+//        Cat cat1 = new Cat("Filemon");
+//        Cat cat2 = new Cat("Bonifacy");
+//
+//        Cat[] cats = {cat, cat1, cat2};
+
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat("Mruczek");
+        cats[1] = new Cat("Filemon");
+        cats[2] = new Cat("Bonifacy");
+
+        int i = 2;
+        for(Cat cat : cats) {
+            //cat.print();
+            //System.out.println(cat.toString());
+            System.out.println(cat);
+            cat.makeSound();
+            cat.eatMice(i++ * i);
+            System.out.println("***");
+        }
+    }
+}
