2f978d16ac011be7f263598790fb92cb35950d1b
Pawel Dudko <paweldudko@outlook.com>
2022-09-24 13:49:38 +0200

    how to run program with program arguments

---
+++ src/pl/sdacademy/java/basic/exercises/day3/Dummy.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.java.basic.exercises.day3;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Dummy {
+
+    public static void main(String[] args) {
+        //System.out.println("Test: " + args[0]);
+        if(args.length > 0) {
+//            DbRepository db = new ...
+//            String[] trans = db.getAllTransactions(args[0]);
+            System.out.println("Pobierz transakcje z wczoraj tj. " + args[0]);
+        } else {
+//            DbRepository db = new ...
+//            String[] trans = db.getAllTransactions(LocalDate.now);
+            System.out.println("Pobierz transakcje z dzisiaj tj. " + LocalDate.now());
+        }
+    }
+}
